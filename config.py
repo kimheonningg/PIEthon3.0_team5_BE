391b929377c5590da8b803c07eed1ac757b82d01
khw2002 <hannahkhw@snu.ac.kr>
2025-06-20 12:01:57 +0900

    add logic for login

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 from functools import lru_cache
 from pathlib import Path
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
-load_dotenv(Path(__file__).resolve().parent / ".env")
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(env_path)
 
 class Settings(BaseSettings):
+    secret_key: str = Field(..., alias="SECRET_KEY")
     mongodb_uri: str = Field(..., env="MONGODB_URI")
     admin_db_name: str = "admin"
 
-    class Config:
-        env_prefix = ""
-        env_file = Path(__file__).parent / ".env"
+    model_config = SettingsConfigDict(
+        env_file=env_path,  
+        extra="ignore",
+        case_sensitive=False
+    )
 
 @lru_cache
 def get_settings() -> Settings:
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, status
-from core.models.registerform import RegisterForm
-from core.auth import register_user
-from core.db import ensure_indexes, init_db
 import uvicorn
+
+from core.models.registerform import RegisterForm
+from core.models.loginform import LoginForm, Token
+from core.auth import (
+    register_user, 
+    authenticate_user,
+    _create_access_token,
+)
+from core.db import ensure_indexes, init_db
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,6 +32,12 @@ async def server_on():
 async def register(user_info: RegisterForm):
     new_id = await register_user(user_info)
     return {"_id": new_id, "message": "registered"}
+    
+@app.post("/login", response_model=Token)
+async def login(credentials: LoginForm):
+    user = await authenticate_user(credentials)
+    access_token = _create_access_token({"sub": str(user["_id"])})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="localhost", port=8000, reload=True)
