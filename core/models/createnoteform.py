083360aa7a9f4dedbd2bedcfbe5b36d2d01efad0
khw2002 <hannahkhw@snu.ac.kr>
2025-06-23 17:00:03 +0900

    add logic to create and upload MedicalNotes

--- core/models/createnoteform.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from core.models.medicalnote import MedicalNoteType
-
-class CreateNoteForm(BaseModel):
-    title: str
-    content: str
-    noteType: MedicalNoteType = "other"
